23e45e3196f0f0a985cda95b21fe4eb325f762b9
ssdv1 <160284219+ssdv1@users.noreply.github.com>
2024-12-02 14:33:29 -0500

    Update bib_get_entries_orcid.py

--- src/make_cv/bib_get_entries_orcid.py
+++ src/make_cv/bib_get_entries_orcid.py
@@ -142,8 +142,7 @@ def bib_get_entries_orcid(bibfile, orcid, years, outputfile):
         print(index)
 
         if index is not None:
-            if (not pub['year'] or entries[index]['year']==pub['year']) and (not pub['journal'] or entries[index]['journal'].lower()==pub['journal'].lower()):
-                #print(entries[index]['year'],pub['year'],entries[index]['journal'].lower(),pub['journal'].lower() )
+            if (not pub['year'] or entries[index]['year']==pub['year']):
                 continue
         
         try:
